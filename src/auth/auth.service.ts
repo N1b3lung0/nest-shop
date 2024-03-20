dfd4037b6648e9e1db427a2884396c3149bb3117
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-03-20 14:19:56 +0100

    Generate jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,12 +10,15 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
 import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
@@ -27,7 +30,10 @@ export class AuthService {
       });
       await this.userRepository.save(user);
       delete user.password;
-      return user;
+      return {
+        ...user,
+        token: this.getJwtToken({ email: user.email }),
+      };
     } catch (error) {
       this.handleDBErrors(error);
     }
@@ -43,7 +49,10 @@ export class AuthService {
       throw new UnauthorizedException('Credentials are not valid (email)');
     if (bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (password)');
-    return user;
+    return {
+      ...user,
+      token: this.getJwtToken({ email: user.email }),
+    };
   }
 
   findAll() {
@@ -62,6 +71,10 @@ export class AuthService {
     return `This action removes a #${id} auth`;
   }
 
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
   private handleDBErrors(error: any): never {
     if (error.code === '23505') throw new BadRequestException(error.detail);
     console.log(error);
